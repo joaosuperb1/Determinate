72dfc53cfa98ea4a6757fef5831921e632306502
João Victor Superbi <57453381+joaosuperb1@users.noreply.github.com>
2023-09-05 14:31:59 -0300

    Determinante.cpp

--- determinante.cpp
+++ determinante.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+
 using namespace std;
 
 void Matriz_2x2(){
@@ -9,21 +10,38 @@ void Matriz_2x2(){
 
     // Preencher Matriz:
     
-    j = 0;
-
+    cout << "Digite os valores:" << endl;
+	
     for(i = 0; i < 2 ; i++){
         for(j = 0; j < 2; j++){
-            cin >> M2[i][j]
+        	
+        	cout << "digite o valor da posicao " << i + 1 << "'" << j + 1 << " : " ;
+            cin >> M2[i][j];
         }
     }
-
-    cout << "Imprimindo Matriz: " << endl;
+	cout << "\n" << endl;
+	
+	// Imprimir Matriz: 
+	
+    cout << "Imprimindo Matriz: \n" << endl;
+    
+    for(i = 0; i < 2 ; i++){
+        for(j = 0; j < 2; j++){
+            
+			if(j < 1){
+				cout << "[" << M2[i][j] << "]";
+			}else{
+				cout << "[" << M2[i][j] << "]" << endl;
+			}
+        
+        }
+    }
     
     // Calculando o determinante de de M2:
 
     int Deter = (M2[0][0]*M2[1][1])-(M2[0][1]*M2[1][0]);
 
-    cout << Deter << endl;
+    cout << "\nO determinante da matriz eh: " << Deter << endl;
 
 }
 
@@ -34,11 +52,30 @@ void Matriz_3x3(){
 
     // Preencher Matriz:
     
-    j = 0;
-
+	cout << "Digite os valores:" << endl;
+	
     for(i = 0; i < 3 ; i++){
-        for(j = 0; j < 2; j++){
-            cin >> M3[i][j]
+        for(j = 0; j < 3; j++){
+        	
+        	cout << "digite o valor da posicao " << i + 1 << "'" << j + 1 << " : " ;
+            cin >> M3[i][j];
+            
+        }
+    }
+    
+	// Imprimir Matriz: 
+	
+    cout << "\nImprimindo Matriz: \n" << endl;
+    
+    for(i = 0; i < 3 ; i++){
+        for(j = 0; j < 3; j++){
+            
+			if(j < 2){
+				cout << "[" << M3[i][j] << "]";
+			}else{
+				cout << "[" << M3[i][j] << "]" << endl;
+			}
+        
         }
     }
 
@@ -46,48 +83,56 @@ void Matriz_3x3(){
 
     int Deter = M3[0][0] * ((M3[1][1]*M3[2][2]) - (M3[2][1]*M3[1][2])) -M3[0][1] * (M3[1][0]
     * M3[2][2] - M3[2][0] * M3[1][2]) + M3[0][2] * (M3[1][0] * M3[2][1] - M3[2][0] * M3[1][1]);
-    cout << Deter << endl;
+    
+    cout << "\nO determinante da matriz eh: " << Deter << endl;
 
 }
 
-/*void Matriz_NxN(tamMatriz){
+/*void Matriz_NxN(){
 
-    int t = tamMatriz
-    int MN[t][t];
+    int t;
+    cin >> t;
+    int 
+    int MN[i][j];
 
     for(i = 0; i < t ; i++){
         for(j = 0; j < 2; j++){
-            cin >> M3[i][j]
+            cin >> M3[i][j];
         }
     }
-
-    
 
 
 }*/
 
 int main(){
 
-    int tamMatriz;
+    int tamMatriz = 1;
 
     cout << "-------------------------------------" << endl;
     cout << "      IF Sudeste MG - Rio Pomba" << endl;
-    cout << "       João Victor - Hernando" << endl;
+    cout << "       Joao Victor - Hernando" << endl;
     cout << "-------------------------------------" << endl;
     cout << "Digite qual o tamanho da matriz:" << endl;
 
-    cin >> tamMatriz;
+	while(tamMatriz == 1){
+		
+		cin >> tamMatriz;
+		
+		if(tamMatriz == 1){
+			cout << "Erro, Matriz de tamanho 1! Escreva novamente o tamanho desejado. :D " << endl;
+		}
+	}
+    
 
     if (tamMatriz == 2){
         Matriz_2x2();
     }else if(tamMatriz == 3){
         Matriz_3x3();
     }else{
-        //Matriz_NxN(tamMatriz);
+    //    Matriz_NxN();
     }
     
 
 
     return 0;
 }
-
