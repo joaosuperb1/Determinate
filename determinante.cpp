c4c163c573b2461008558d4fe5932cd8e863bdae
João Victor Superbi <57453381+joaosuperb1@users.noreply.github.com>
2023-09-14 15:45:28 -0300

    Update determinante.cpp

--- determinante.cpp
+++ determinante.cpp
@@ -117,6 +117,98 @@ void Matriz_3x3(){
 
 }
 
+void Matriz_4x4(){
+
+//    int Deter;
+    int M4[4][4];
+    int M3Apoio[3][3];
+    int i, j;
+
+    // Preencher Matriz:
+    
+	cout << "Digite os valores:" << endl;
+	
+    for(i = 0; i < 4 ; i++){
+        for(j = 0; j < 4; j++){
+        	cout << "digite o valor da posicao " << i + 1 << "'" << j + 1 << " : " ;
+            cin >> M4[i][j];
+            
+        }
+    }
+    
+	// Imprimir Matriz: 
+	
+    cout << "\nImprimindo Matriz: \n" << endl;
+    
+    for(i = 0; i < 4 ; i++){
+        for(j = 0; j < 4; j++){
+            
+			if(j < 3){
+				cout << "[" << M4[i][j] << "]";
+			}else{
+				cout << "[" << M4[i][j] << "]" << endl;
+			}
+        
+        }
+    }
+
+    // Fazendo a Matriz de apoio
+
+    int k = 0;
+    int l = 0;
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            cout << i << " " << j << endl;
+            if(i == 0 || j == 0){
+                //int teste;
+                cout << "i ou j = 0\n";
+                //cin >> teste;
+                //cout << teste << "teste" << endl;
+                
+            }else{
+
+                M3Apoio[k][l] = M4[i][j];
+                cout << "imprimindo m3apoio " <<  M3Apoio[k][l] << endl;
+                
+            }
+            cout << "Imprime Apoio de novo" <<  M3Apoio[k][l] << endl;
+            l++;
+
+        }
+        cout << "Imprime Apoio de novo" << M3Apoio[k][l] << endl;
+        k++;
+    }
+
+    // Imprimir Matriz Apoio: 
+	
+    cout << "\nImprimindo Matriz de apoio: \n" << endl;
+    
+    for(k = 0; k < 3 ; k++){
+        for(l = 0; l < 3; l++){
+            
+			if(l < 2){
+                int t;
+				cout << "[" << M3Apoio[k][l] << "]";
+                cin >> t;
+			}else{
+                int t;
+				cout << "[" << M3Apoio[k][l] << "]" << endl;
+                cin >> t;
+			}
+        
+        }
+    }
+
+
+    // Calculando o determinante de de M3:
+
+    
+    
+    //cout << "\nO determinante da matriz eh: " << Deter << endl;
+
+}
+
 /*void Matriz_NxN(){
 
     int t;
@@ -147,7 +239,7 @@ int main(){
 		
 		cin >> tamMatriz;
 		
-		if(tamMatriz == 1){
+		if(tamMatriz == 0){
 			cout << "Erro, Matriz de tamanho 0! Escreva novamente o tamanho desejado. :D " << endl;
 		}
 	}
@@ -159,6 +251,8 @@ int main(){
         Matriz_2x2();
     }else if(tamMatriz == 3){
         Matriz_3x3();
+    }else if(tamMatriz == 4){
+        Matriz_4x4();
     }else{
     //    Matriz_NxN();
     }
